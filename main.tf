1948318efb8e800a37c655e6165af4e454b9cb17
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-11 06:35:08 +0000

    Ads vars

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
 resource "tfe_workspace" "test" {
-  name         = "rodasnet-test"
-  organization = "rodasnet"
-  tag_names    = ["test", "app"]
+  name         = var.workspace_name
+  organization = var.org_name
+
+  # vcs_repo {
+  #   identifier = var.vsc_identifier
+  #   oauth_token_id = 
+  # }
 }
 
+# resource "tfe_oauth_client" "test" {
+#   name             = "my-github-oauth-client"
+#   organization     = "rodasnet"
+#   api_url          = "https://api.github.com"
+#   http_url         = "https://github.com"
+#   oauth_token      = "my-vcs-provider-token"
+#   service_provider = "github"
+# }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
 variable "tf_token" {
   type = string
 }
+
+variable  "workspace_name" {
+  type = string
+  default = "new-workspace"
+}
+
+variable "org_name" {
+  type = string
+  default = "rodasnet"
+}
+
+variable "vsc_identifier" {
+  type = string
+}
