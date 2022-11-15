2182d1e962764329e67855111f9fabb35881a797
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-15 07:40:43 +0000

    Test optional branch

--- aws-acct-dev-02.tf
+++ aws-acct-dev-02.tf
@@ -2,7 +2,10 @@ module "aws-acct-dev-01" {
   source = "./module/workspace"
 
   organization      = var.organization
-  workspace_name    = "aws-acct-dev-01"
+  workspace_name    = "aws-acct-dev-02"
   repo_name         = "terraform-rodasnet.com"
   oauth_token       = var.github_token
+  
+  // Optional variable
+  branch            = "feature-branch"
 }
