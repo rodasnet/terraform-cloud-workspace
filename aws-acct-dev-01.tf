9bdda0bfdd25ed41e1b9c492fcd57338d6599a6c
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 22:27:25 -0800

    refactor my-workspace_settings

--- aws-acct-dev-01.tf
+++ aws-acct-dev-01.tf
@@ -1,10 +1,10 @@
-module "aws-acct-dev-01" {
-  source = "./module/workspace"
+# module "aws-acct-dev-01" {
+#   source = "./module/workspace"
 
-  workspace_settings = {
-    organization = "rodasnet"
-    description = "Second something really special"
-    repo_name = "terraform-rodasnet.com"
-    workspace_name = "aws-acct-dev-01"
-  }
-}
+#   workspace_settings = {
+#     organization = "rodasnet"
+#     description = "Second something really special"
+#     repo_name = "terraform-rodasnet.com"
+#     workspace_name = "aws-acct-dev-01"
+#   }
+# }
--- aws-acct-dev-02.tf
+++ aws-acct-dev-02.tf
@@ -1,10 +1,14 @@
 module "aws-acct-dev-02" {
   source = "./module/workspace"
 
-  workspace_settings = {
-    organization = "rodasnet"
-    description = "Second something really special"
-    repo_name = "terraform-rodasnet.com"
-    workspace_name = "aws-acct-dev-02"
-  }
+  workspace_settings = var.my-workspace_settings
+}
+
+variable "my-workspace_settings" {
+  type = object({
+    organization        = "rodasnet"
+    description         = "Second something really special"
+    repo_name           = "terraform-rodasnet.com"
+    workspace_name      = "aws-acct-dev-02"
+  })
 }
