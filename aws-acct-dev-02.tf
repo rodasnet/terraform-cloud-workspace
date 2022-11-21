ed3cde13a8dd67e34e901dfdb64dd5743a7bd7d1
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 22:41:31 -0800

    Use inline var for workspace settngs

--- aws-acct-dev-02.tf
+++ aws-acct-dev-02.tf
@@ -1,21 +1,27 @@
 module "aws-acct-dev-02" {
   source = "./module/workspace"
 
-  workspace_settings = var.my-workspace_settings
-}
-
-variable "my-workspace_settings" {
-  # type = object({
-  #   organization    = optional(string)
-  #   workspace_name  = optional(string)
-  #   repo_name       = optional(string)
-  #   description     = optional(string)
-  # })
-
-  default = {
+  # workspace_settings = var.my-workspace_settings
+  workspace_settings = {
     organization        = "rodasnet"
     description         = "Second something really special"
     repo_name           = "terraform-rodasnet.com"
     workspace_name      = "aws-acct-dev-02"
   }
 }
+
+# variable "my-workspace_settings" {
+#   # type = object({
+#   #   organization    = optional(string)
+#   #   workspace_name  = optional(string)
+#   #   repo_name       = optional(string)
+#   #   description     = optional(string)
+#   # })
+
+#   default = {
+#     organization        = "rodasnet"
+#     description         = "Second something really special"
+#     repo_name           = "terraform-rodasnet.com"
+#     workspace_name      = "aws-acct-dev-02"
+#   }
+# }
