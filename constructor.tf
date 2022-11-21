2dc71580588fd16eb7106b643e0e7b29f6da031b
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 20:22:22 -0800

    test constructor count 0

--- constructor.tf
+++ constructor.tf
@@ -1,13 +1,10 @@
-# module "constructor" {
-#   source = "./module/workspace"
+module "constructor" {
+  source = "./module/workspace"
 
-#   count = 0
+  count = 0
 
-#   organization      = var.organization
-#   workspace_name    = "constructor"
-#   repo_name         = "terraform-rodasnet.com"
-#   oauth_token       = var.github_token == "" ? null : var.github_token
-  
-
-# }
-
+  organization      = var.organization
+  workspace_name    = "constructor"
+  repo_name         = "terraform-rodasnet.com"
+  oauth_token       = var.github_token == "" ? null : var.github_token
+}
