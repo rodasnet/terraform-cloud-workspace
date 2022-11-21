dd1893efe07d7bc8785ea0ed23d4374683ef347a
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 23:39:31 -0800

    Update variables of oauth client

--- github-oauth.tf
+++ github-oauth.tf
@@ -1,7 +1,7 @@
 resource "tfe_oauth_client" "oauth_client" {
   name             = "${var.organization}-oauth-client"
   organization     = var.organization
-  oauth_token      = var.oauth_token
+  oauth_token      = var.github_token
   api_url          = var.github_oauth_client.api_url
   http_url         = var.github_oauth_client.http_url
   service_provider = var.github_oauth_client.service_provider
