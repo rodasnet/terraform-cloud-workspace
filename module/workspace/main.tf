84f3ba6c67858ec38fda5558512d3007b06f6609
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 19:23:25 -0800

    Count 0 module

--- module/workspace/main.tf
+++ module/workspace/main.tf
@@ -1,4 +1,5 @@
 resource "tfe_workspace" "workspace" {
+  count                 = 0
   name                  = var.workspace_name
   organization          = var.organization
   description           = var.description == "" ? "Workspace ${var.workspace_name}" : var.description
@@ -13,6 +14,29 @@ resource "tfe_workspace" "workspace" {
     branch          = var.branch
   }
 }
+
+# module "constructor" {
+#   source = "./module/tfe_workspace"
+
+  
+# }
+
+tfe_variable "workspace_settings" "workspace" {
+  name                  = var.workspace_name
+  organization          = var.organization
+  description           = var.description == "" ? "Workspace ${var.workspace_name}" : var.description
+  auto_apply            = var.auto_apply
+  file_triggers_enabled = var.file_triggers_enabled
+  ssh_key_id            = var.ssh_key_id
+  allow_destroy_plan    = var.allow_destroy_plan
+
+  vcs_repo {
+    identifier      = "${var.organization}/${var.repo_name}"
+    oauth_token_id  = tfe_oauth_client.oauth_client.oauth_token_id
+    branch          = var.branch
+  }
+}
+
 
 resource "tfe_oauth_client" "oauth_client" {
   name             = "${var.workspace_name}-oauth-client"
