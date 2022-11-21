0e186dedca302a8aac637b9884e66bca6c6f2530
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 20:10:06 -0800

    Test workspace with no vcs

--- constructor.tf
+++ constructor.tf
@@ -1,30 +1,13 @@
-module "constructor" {
-  source = "./module/workspace"
+# module "constructor" {
+#   source = "./module/workspace"
 
-  organization      = "rodasnet"
-  workspace_name    = "worksace-with-constructor-01"
-  repo_name         = "terraform-rodasnet.com"
-  oauth_token       = var.github_token == "" ? null : var.github_token
+#   count = 0
+
+#   organization      = var.organization
+#   workspace_name    = "constructor"
+#   repo_name         = "terraform-rodasnet.com"
+#   oauth_token       = var.github_token == "" ? null : var.github_token
   
-  // Optional variable
-  branch            = "feature-branch"
 
-#   tfe_variable_sensitive_map = var.sensitives
-#   tfe_variable_public_map = var.workspace_vars
-}
+# }
 
-variable "sensitives" {
-  type = map
-  default = {
-    COLOR = "Magic"
-    BANK_ACCOUNT = "292929292993"
-  }
-}
-
-variable "workspace_vars" {
-  type = map
-  default = {
-    LOCATION = "West Coast"
-    TIER = "Premium"
-  }
-}
