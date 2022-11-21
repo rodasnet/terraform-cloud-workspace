41b3760bb123d9792c18bbfd8c296ab7cfbe47bc
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-21 02:35:26 -0800

    Test sensitivity for token id

--- aws-acct-dev-03.tf
+++ aws-acct-dev-03.tf
@@ -1,15 +1,15 @@
-module "aws-acct-dev-03" {
-  source = "./module/workspace"
-  organization = var.organization
+# module "aws-acct-dev-03" {
+#   source = "./module/workspace"
+#   organization = var.organization
 
-  oauth_token_2 = var.github_token
+#   oauth_token_2 = var.github_token
 
-  workspace_settings = {
-    workspace_name      = "aws-acct-dev-03"
-    description         = "Third something really special"
-    vcs = {
-      repo_name = "terraform-rodasnet.com"
-      oauth_token_id = tfe_oauth_client.oauth_client.oauth_token_id
-    }
-  }
-}
+#   workspace_settings = {
+#     workspace_name      = "aws-acct-dev-03"
+#     description         = "Third something really special"
+#     vcs = {
+#       repo_name = "terraform-rodasnet.com"
+#       oauth_token_id = tfe_oauth_client.oauth_client.oauth_token_id
+#     }
+#   }
+# }
