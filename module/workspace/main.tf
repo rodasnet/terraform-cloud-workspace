649d077cd58eb10f838ba7ae6f6e04ab0fe91f61
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 19:26:05 -0800

    Count 0 module

--- module/workspace/main.tf
+++ module/workspace/main.tf
@@ -21,21 +21,21 @@ resource "tfe_workspace" "workspace" {
   
 # }
 
-tfe_variable "workspace_settings" "workspace" {
-  name                  = var.workspace_name
-  organization          = var.organization
-  description           = var.description == "" ? "Workspace ${var.workspace_name}" : var.description
-  auto_apply            = var.auto_apply
-  file_triggers_enabled = var.file_triggers_enabled
-  ssh_key_id            = var.ssh_key_id
-  allow_destroy_plan    = var.allow_destroy_plan
+# tfe_variable "workspace_settings" "workspace" {
+#   name                  = var.workspace_name
+#   organization          = var.organization
+#   description           = var.description == "" ? "Workspace ${var.workspace_name}" : var.description
+#   auto_apply            = var.auto_apply
+#   file_triggers_enabled = var.file_triggers_enabled
+#   ssh_key_id            = var.ssh_key_id
+#   allow_destroy_plan    = var.allow_destroy_plan
 
-  vcs_repo {
-    identifier      = "${var.organization}/${var.repo_name}"
-    oauth_token_id  = tfe_oauth_client.oauth_client.oauth_token_id
-    branch          = var.branch
-  }
-}
+#   vcs_repo {
+#     identifier      = "${var.organization}/${var.repo_name}"
+#     oauth_token_id  = tfe_oauth_client.oauth_client.oauth_token_id
+#     branch          = var.branch
+#   }
+# }
 
 
 resource "tfe_oauth_client" "oauth_client" {
