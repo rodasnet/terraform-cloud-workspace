4e96878464913e7ddb0e6ac5c3753aefc4b52b10
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 22:05:00 -0800

    Add missing var

--- aws-acct-dev-01.tf
+++ aws-acct-dev-01.tf
@@ -2,7 +2,8 @@ module "aws-acct-dev-01" {
   source = "./module/workspace"
 
   workspace_settings = {
-    description = "Something really special"
+    organization = "rodasnet"
+    description = "Second something really special"
     repo_name = "terraform-rodasnet.com"
     workspace_name = "aws-acct-dev-01"
   }
