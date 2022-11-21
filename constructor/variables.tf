645a08c68cffdad232a5cb2011cc911db392b59e
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 21:03:26 -0800

    Add default

--- constructor/variables.tf
+++ constructor/variables.tf
@@ -7,11 +7,13 @@ variable "organization" {
 variable "tf_token" {
   type      = string
   sensitive = true
+    default = null
 }
 
 variable "github_token" {
   type      = string
   sensitive = true
+    default = null
 }
 
 
