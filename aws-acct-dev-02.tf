38b31e6de10e0b8898fbf58267af4de15de0a74a
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-20 23:14:14 -0800

    Readd workspaces

--- aws-acct-dev-02.tf
+++ aws-acct-dev-02.tf
@@ -1,11 +1,11 @@
-# module "aws-acct-dev-02" {
-#   source = "./module/workspace"
+module "aws-acct-dev-02" {
+  source = "./module/workspace"
 
-#   # workspace_settings = var.my-workspace_settings
-#   workspace_settings = {
-#     organization        = "rodasnet"
-#     description         = "Second something really special"
-#     repo_name           = "terraform-rodasnet.com"
-#     workspace_name      = "aws-acct-dev-02"
-#   }
-# }
+  # workspace_settings = var.my-workspace_settings
+  workspace_settings = {
+    organization        = "rodasnet"
+    description         = "Second something really special"
+    repo_name           = "terraform-rodasnet.com"
+    workspace_name      = "aws-acct-dev-02"
+  }
+}
