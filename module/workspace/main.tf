c5ceecc92c3a287863205c6affc61252a20a7b06
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-28 12:46:21 -0800

    Add all optional var for tfe_workspace resource

--- module/workspace/main.tf
+++ module/workspace/main.tf
@@ -3,6 +3,23 @@ resource "tfe_workspace" "workspace" {
   name              = var.workspace_settings.workspace_name
   description       = var.workspace_settings.description
 
+  agent_pool_id   = var.workspace_settings.agent_pool_id
+  allow_destroy_plan    = var.workspace_settings.allow_destroy_plan
+  auto_apply            = var.workspace_settings.auto_apply
+  execution_mode        = var.workspace_settings.execution_mode
+  assessments_enabled   = var.workspace_settings.assessments_enabled
+  file_triggers_enabled   = var.workspace_settings.file_triggers_enabled
+  global_remote_state     = var.workspace_settings.global_remote_state
+  queue_all_runs          = var.workspace_settings.queue_all_runs
+  speculative_enabled     = var.workspace_settings.speculative_enabled
+  structured_run_output_enabled       = var.workspace_settings.structured_run_output_enabled
+  ssh_key_id                          = var.workspace_settings.ssh_key_id
+  tag_names                           = var.workspace_settings.tag_names
+  terraform_version                   = var.workspace_settings.terraform_version
+  trigger_prefixes                    = var.workspace_settings.trigger_prefixes
+  trigger_patterns                    = var.workspace_settings.trigger_patterns
+  working_directory                   = var.workspace_settings.working_directory
+
   dynamic vcs_repo {
     for_each = var.workspace_settings.vcs != null ? [1] : []
     content {
@@ -10,7 +27,7 @@ resource "tfe_workspace" "workspace" {
       oauth_token_id  = var.workspace_settings.vcs.oauth_token_id
       branch          = var.workspace_settings.vcs.branch
     }
-  }  
+  } 
 }
 
 # resource "tfe_variable" "sensitive" {
