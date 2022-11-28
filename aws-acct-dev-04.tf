98f1f9b21bfb6fedd1932402a921a2dfdd0a540b
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-28 13:00:10 -0800

    Test TF version for new workspace

--- aws-acct-dev-04.tf
+++ aws-acct-dev-04.tf
@@ -1,15 +1,10 @@
-# module "aws-acct-dev-04" {
-#   source = "./module/workspace"
-#   organization = var.organization
+module "aws-acct-dev-04" {
+  source = "./module/workspace"
+  global_settings = var.global_settings
 
-#   oauth_token_2 = var.github_token
-
-#   workspace_settings = {
-#     workspace_name      = "aws-acct-dev-04"
-#     description         = "Fourth something really special"
-#     vcs = {
-#       repo_name = "terraform-rodasnet.com"
-#       oauth_token_id = tfe_oauth_client.oauth_client.oauth_token_id
-#     }
-#   }
-# }
+  workspace_settings = {
+    workspace_name      = "aws-acct-dev-04"
+    description         = "Fourth something really special"
+    terraform_version   = "1.0.0"
+  }
+}
