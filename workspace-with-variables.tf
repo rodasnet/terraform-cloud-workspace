d4f36c44276d8b97adc71de9eb63a9d331c64da2
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-11-28 20:46:37 -0800

    TFE Workspace module with all options

--- workspace-with-variables.tf
+++ workspace-with-variables.tf
@@ -4,32 +4,11 @@ module "workspace-with-variables" {
 
   workspace_settings = {
     workspace_name                  = "workspace-with-variables"
-    description                     = "Example TFE Workspace module with all options"
-    agent_pool_id                   = tfe_agent_pool.test-agent-pool.id
-    # allow_destroy_plan              = ""
-    # auto_apply                      = ""
-    # execution_mode                  = ""
-    # assessments_enabled             = ""
-    # file_triggers_enabled           = ""
-    # global_remote_state             = ""
-    # queue_all_runs                  = ""
-    # speculative_enabled             = ""
-    # structured_run_output_enabled   = ""
-    # ssh_key_id                      = ""
-    # tag_names                       = ""
-    # terraform_version               = ""
-    # trigger_prefixes                = ""
-    # trigger_patterns                = ""
-    # working_directory               = ""
+    description                     = "TFE Workspace module with workspace variables."
   }
   
   tfe_variable_public_map = {
     LOCATION    = "West Coast"
     TIER        = "Premium"
   }
-}
-
-resource "tfe_agent_pool" "test-agent-pool" {
-  name         = "my-agent-pool-name"
-  organization = var.global_settings.organization
 }
