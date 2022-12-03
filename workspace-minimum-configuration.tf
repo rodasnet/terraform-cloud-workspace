cdabbd121b9d046789ed879776e97c3ad1f93bff
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-12-02 16:07:09 -0800

    Test individual variable style

--- workspace-minimum-configuration.tf
+++ workspace-minimum-configuration.tf
@@ -2,6 +2,8 @@ module "workspace-minimum-configuration" {
   source          = "./module/workspace"
   global_settings = var.global_settings
 
+  my_description = "A new description ."
+
   workspace_settings = {
     workspace_name = "workspace-minimum-configuration"
     description    = "Second something really special"
