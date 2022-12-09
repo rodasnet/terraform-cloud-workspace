9b7c278368529531befdc6b4a490d58c4b30b05d
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-12-09 14:11:29 -0800

    Update workspace-test-input-variable.tf

--- workspace-test-input-variable.tf
+++ workspace-test-input-variable.tf
@@ -1,12 +1,11 @@
-module "workspace-with-tf-version" {
+module "workspace-test-input-variable" {
   source          = "./module/workspace"
   global_settings = var.global_settings
   
   my_description = "This is a description" 
 
   workspace_settings = {
-    workspace_name    = "workspace-with-tf-version"
-    description       = "Fourth something really special"
+    workspace_name    = "workspace-test-input-variable"
     terraform_version = "1.0.0"
   }
 }
