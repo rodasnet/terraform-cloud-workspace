3a1b355aa3b7e3d255b049344d2927eb4eb58757
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-12-12 19:40:37 -0800

    Add default null for remaining variables

--- module/workspace/variables.tf
+++ module/workspace/variables.tf
@@ -39,13 +39,13 @@ variable "execution_mode" {
 
 variable "assessments_enabled" {
   type = bool
-  
+  default = null
   description = "(Optional) Whether to regularly run health assessments such as drift detection on the workspace. Defaults to false."
 }
 
 variable "file_triggers_enabled" {
   type = bool
-  
+  default = null
   description = "(Optional) Whether to filter runs based on the changed files in a VCS push. Defaults to true. If enabled, the working directory and trigger prefixes describe a set of paths which must contain changes for a VCS push to trigger a run. If disabled, any push will trigger a run."
 }
 
@@ -57,7 +57,7 @@ variable "global_remote_state" {
 
 variable "remote_state_consumer_ids" {
   type = list(string)
-  
+  default = null
   description = "(Optional) The set of workspace IDs set as explicit remote state consumers for the given workspace."
 }
 
@@ -69,60 +69,61 @@ variable "remote_state_consumer_ids" {
 
 variable "queue_all_runs" {
   type = bool
-  
+  default = null
   description = "(Optional) Whether the workspace should start automatically performing runs immediately after its creation. Defaults to true. When set to false, runs triggered by a webhook (such as a commit in VCS) will not be queued until at least one run has been manually queued. Note: This default differs from the Terraform Cloud API default, which is false. The provider uses true as any workspace provisioned with false would need to then have a run manually queued out-of-band before accepting webhooks."
 }
 
 variable "speculative_enabled" {
   type = bool
-  
+  default = null
   description = "(Optional) Whether this workspace allows speculative plans. Defaults to true. Setting this to false prevents Terraform Cloud or the Terraform Enterprise instance from running plans on pull requests, which can improve security if the VCS repository is public or includes untrusted contributors."
 }
 
 variable "structured_run_output_enabled" {
   type = bool
-  
+  default = null
   description = "(Optional) Whether this workspace should show output from Terraform runs using the enhanced UI when available. Defaults to true. Setting this to false ensures that all runs in this workspace will display their output as text logs."
 }
 
 variable "ssh_key_id" {
   type = string
-  
+  default = null
   description = "(Optional) The ID of an SSH key to assign to the workspace."
 }
 
 variable "tag_names" {
   type = list(string)
-  
+  default = null
   description = "(Optional) A list of tag names for this workspace. Note that tags must only contain lowercase letters, numbers, colons, or hyphens."
 }
 
 variable "terraform_version" {
   type = string
-  
+  default = null
   description = "(Optional) The version of Terraform to use for this workspace. This can be either an exact version or a version constraint (like ~> 1.0.0); if you specify a constraint, the workspace will always use the newest release that meets that constraint. Defaults to the latest available version."
 }
 
 variable "trigger_prefixes" {
   type = list(string)
-  
+  default = null
   description = "(Optional) List of repository-root-relative paths which describe all locations to be tracked for changes."
 }
 
 variable "trigger_patterns" {
   type = list(string)
-  
+  default = null
   description = "(Optional) List of glob patterns that describe the files Terraform Cloud monitors for changes. Trigger patterns are always appended to the root directory of the repository. Mutually exclusive with trigger-prefixes. Only available for Terraform Cloud."
 }
 
 variable "working_directory" {
   type = string
-  
+  default = null
   description = "(Optional) A relative path that Terraform will execute within. Defaults to the root of your repository."
 }
 
 variable "vcs_repo" {
   type = map
+  default = null
   description = "(Optional) Settings for the workspace's VCS repository, enabling the UI/VCS-driven run workflow. Omit this argument to utilize the CLI-driven and API-driven workflows, where runs are not driven by webhooks on your VCS provider."
 }
 
