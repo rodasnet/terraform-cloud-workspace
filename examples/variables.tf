9528700b569bcdd1edf020f4c9222f82af80231f
Daniel Rodas <daniel.rodas1@hotmail.com>
2022-12-13 01:22:42 -0800

    Shuffle files

---
+++ examples/variables.tf
@@ -0,0 +1,25 @@
+// Need for TFE provider configuration
+variable "tf_token" {
+  type      = string
+  sensitive = true
+  default   = null
+}
+
+variable "github_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "github_oauth_client" {
+  type = map(any)
+  default = {
+    api_url          = "https://api.github.com"
+    http_url         = "https://github.com"
+    service_provider = "github"
+  }
+}
+
+variable "organization" {
+  type = string
+  default = "rodasnet"
+}
