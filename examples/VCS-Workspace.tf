06fdb65125ed46a93a04329c1d82befd4e9a47a7
Daniel Rodas <daniel.rodas1@hotmail.com>
2023-01-03 20:57:49 -0800

    Update VCS-Workspace.tf

--- examples/VCS-Workspace.tf
+++ examples/VCS-Workspace.tf
@@ -6,7 +6,7 @@ module "VCS-Workspace" {
   description                   = "Terraform workspace with VCS configuration"
 
   vcs_repo = {
-      identifier      = "fake-org-1/terraform-rodasnet.com"
+      identifier      = "daniel-rodas/terraform-rodasnet.com"
       oauth_token_id = tfe_oauth_client.oauth_client.oauth_token_id
     }
 }
