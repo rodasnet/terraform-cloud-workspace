5fc3450a27edeeb8b297d79728e106a6150d8834
Daniel Rodas <daniel.rodas1@hotmail.com>
2023-01-12 23:02:02 -0800

    To list me now

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   default_tags = {
-    Terraform = "TfcManagedWorkspace"
+    Terraform = "tfc-managed"
   }
 
   tags_merged = concat(tolist(values(local.default_tags)), tolist(var.tag_names == null ? [] : var.tag_names))
