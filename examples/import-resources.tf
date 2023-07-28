9f93afb73d3022f6f6de749f9ee9d9a94f8afbcb
Daniel Rodas <daniel.rodas1@hotmail.com>
2023-07-27 17:23:01 -0700

    Create import-resources.tf

---
+++ examples/import-resources.tf
@@ -0,0 +1,16 @@
+module "Workspace-Import-Resources" {
+  source          = "../"
+  
+  organization                  = var.organization
+  name                          = "Workspace-Import-Resources"
+  description                   = "Terraform workspace to import resources into Terraform state"
+
+  vcs_repo = {
+      identifier      = "rodasnet/terraform-import-resources"
+      oauth_token_id  = var.github_oauth_token_id
+    }
+
+  variable_set_names = [
+    "tfe-api-token"
+  ]
+}
