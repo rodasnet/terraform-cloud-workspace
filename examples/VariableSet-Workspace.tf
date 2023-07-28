08b6751a06f1da0a875d365ebfd58558d847bc64
Daniel Rodas <daniel.rodas1@hotmail.com>
2023-07-27 17:26:19 -0700

    Update VariableSet-Workspace.tf

--- examples/VariableSet-Workspace.tf
+++ examples/VariableSet-Workspace.tf
@@ -10,10 +10,10 @@ module "VariableSet-Workspace" {
     "tfe-api-token"
   ]
 
-  tfe_variable_sensitive_map = {
-    "a_key" = var.another_key
-    "b_key" = var.something_special
-  }
+  # tfe_variable_sensitive_map = {
+  #   "a_key" = var.another_key
+  #   "b_key" = var.something_special
+  # }
 
   # tfe_variable_sensitive_map = {
   #   "a_key" = data.tfe_variable_set.varset_azure_spn.variable_ids 
