a70469b2a02a957d70131d6474144528c6de9386
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-01-11 23:23:27 -0800

    Update run_task.tf

--- run_task.tf
+++ run_task.tf
@@ -1,8 +1,29 @@
 
-# resource "tfe_workspace_run_task" "work_task" {
-#   workspace_id = ""
-#   enforcement_level =  ""
-#   task_id = ""
-#   stage = ""
+#   for_each = { for config in var.run_task_config_list : config => config.value }
+# resource "tfe_workspace_run_task" "example" {
+#   workspace_id      = resource.tfe_workspace.example.id
+#   task_id           = resource.tfe_organization_run_task.example.id
+#   enforcement_level = "advisory"
 # }
 
+resource "tfe_workspace_run_task" "workspace_task_synk" {
+    count = var.create_runtask == trye ? 1 : 0
+    workspace_id = tfe_workspace.workspace.id
+    task_id = data.tfe_organization_run_task.synk.id
+    enforcement_level = "advisory"
+}
+
+variable "run_task_config_list" {
+  type = list(object({
+    enforcement_level = string
+    task_id = string
+    stage = optional(string)
+  }))
+
+  default = null
+}
+
+variable "create_runtask" {
+  type = bool
+  default = false
+}
