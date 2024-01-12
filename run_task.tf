fe6535b6f5c20ca8c209975d573c97ae983ad834
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-01-11 21:33:08 -0800

    Update run_task.tf

--- run_task.tf
+++ run_task.tf
@@ -1,11 +1,3 @@
-resource "tfe_organization_run_task" "org_task" {
-    organization = var.organization
-    url = "https://api.snyk.io/v1/terraform-cloud/3997a565-cf35-4b0e-8c8d-b59b2f14370a"
-    name = "RunTaskOrgSynk"
-    enabled = true
-    description = "Sync organazational run task."
-    hmac_key = var.synk_hmac_key
-}
 
 # resource "tfe_workspace_run_task" "work_task" {
 #   workspace_id = ""
