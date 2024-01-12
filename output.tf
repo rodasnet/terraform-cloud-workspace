a33e253552045ce896fa0399f7b4f9ceaf7e4305
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-01-11 21:40:59 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,23 @@
 output "workspace_id" {
   value = tfe_workspace.workspace.id
 }
+
+output "org_task_category" {
+  value = data.tfe_organization_run_task.synk.category
+}
+
+output "org_task_description" {
+  value = data.tfe_organization_run_task.synk.description
+}
+
+output "org_task_enabled" {
+  value = data.tfe_organization_run_task.synk.enabled
+}
+
+output "org_task_id" {
+  value = data.tfe_organization_run_task.synk.id
+}
+
+output "org_task_url" {
+  value = data.tfe_organization_run_task.synk.url
+}
