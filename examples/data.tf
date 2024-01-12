464370ebb134b4fa020b67e26327b5f196c3d5ac
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-01-11 21:57:14 -0800

    Create data.tf

---
+++ examples/data.tf
@@ -0,0 +1,27 @@
+output "org_task_name" {
+  value = tfe_organization_run_task.synk.name
+}
+
+# output "org_task_organization" {
+#   value = tfe_organization_run_task.synk.organization
+# }
+
+# output "org_task_category" {
+#   value = data.tfe_organization_run_task.synk.category
+# }
+
+# output "org_task_description" {
+#   value = data.tfe_organization_run_task.synk.description
+# }
+
+# output "org_task_enabled" {
+#   value = data.tfe_organization_run_task.synk.enabled
+# }
+
+# output "org_task_id" {
+#   value = data.tfe_organization_run_task.synk.id
+# }
+
+# output "org_task_url" {
+#   value = data.tfe_organization_run_task.synk.url
+# }
