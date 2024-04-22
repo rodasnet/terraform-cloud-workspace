dea0a2adc0611034992813603ac0c66416d080b5
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-22 07:25:44 -0700

    Create project.tf

---
+++ project.tf
@@ -0,0 +1,49 @@
+
+resource "tfe_project_policy_set" "project_policy_set" {
+
+  count         = length(var.project_policy_set_list)
+  policy_set_id = var.project_policy_set_list[count].policy_set_id
+  project_id    = var.project_policy_set_list[count].project_id
+}
+
+variable "project_policy_set_list" {
+  type = list(object({
+    policy_set_id = string
+    project_id    = string
+  }))
+
+  default = []
+}
+
+resource "tfe_project_variable_set" "project_variable_set" {
+
+  count = length(var.project_variable_set_list)
+
+  variable_set_id = var.project_variable_set_list[count].variable_set_id
+  project_id      = var.project_variable_set_list[count].project_id
+}
+
+variable "project_variable_set_list" {
+  type = list(object({
+    variable_set_id = string
+    project_id      = string
+  }))
+  default = []
+}
+
+resource "tfe_team_project_access" "project_access" {
+    count = length(var.project_access)
+
+    access = var.project_access[count].access
+    team_id = var.project_access[count].team_id
+    project_id = var.project_access[count].project_id
+}
+
+variable "project_access" {
+  type = list(object({
+    access = string
+    team_id = string
+    project_id = string 
+  }))
+  default = []
+}
