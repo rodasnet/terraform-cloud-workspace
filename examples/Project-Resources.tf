1ad8df17b0062c46082133381a4240e7007776b0
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-22 16:06:29 -0700

    WiP Project and related resources

---
+++ examples/Project-Resources.tf
@@ -0,0 +1,32 @@
+module "Project-Standalone-1" {
+  source          = "../"
+
+  create_workspace = false
+
+   project_list = [
+    {
+      organization = var.organization
+      name = "MyFirstTfcProject1"
+    }
+   ]
+}
+
+module "Project-PolicySet-1" {
+  source          = "../"
+
+  create_workspace = false
+
+   project_list = [
+    {
+      organization = var.organization
+      name = "Project-PolicySet-1"
+    }
+   ]
+
+   project_policy_set_list = [
+    {
+      policy_set_id
+      project_id
+    }
+   ]
+}
--- project.tf
+++ project.tf
@@ -15,14 +15,43 @@ variable "project_list" {
   default = []
 }
 
-resource "tfe_project_policy_set" "project_policy_set" {
+resource "tfe_policy_set" "policy_set" {
 
-  count         = length(var.project_policy_set_list)
-  policy_set_id = var.project_policy_set_list[count.index].policy_set_id
-  project_id    = var.project_policy_set_list[count.index].project_id
+  count = length(var.create_project_policy_set_list)
+
+  name                = var.create_project_policy_set_list[count.index].name
+  description         = var.create_project_policy_set_list[count.index].description
+  organization        = var.create_project_policy_set_list[count.index].organization
+  kind                = var.create_project_policy_set_list[count.index].kind
+  agent_enabled       = var.create_project_policy_set_list[count.index].agent_enabled
+  policy_tool_version = var.create_project_policy_set_list[count.index].policy_tool_version
+  policies_path       = var.create_project_policy_set_list[count.index].policies_path
+  workspace_ids       = var.create_project_policy_set_list[count.index].workspace_ids
+
+  # vcs_repo {
+  #   identifier         = "my-org-name/my-policy-set-repository"
+  #   branch             = "main"
+  #   ingress_submodules = false
+  #   oauth_token_id     = tfe_oauth_client.test.oauth_token_id
+  # }
 }
 
-variable "project_policy_set_list" {
+resource "tfe_project_policy_set" "created" {
+
+  count         = length(var.create_project_policy_set_list)
+
+  policy_set_id = tfe_policy_set.policy_set[count.index].policy_set_id
+  project_id    = var.create_project_policy_set_list[count.index].project_id
+}
+
+resource "tfe_project_policy_set" "linked" {
+
+  count         = length(var.link_project_policy_set_list)
+  policy_set_id = var.link_project_policy_set_list[count.index].policy_set_id
+  project_id    = var.link_project_policy_set_list[count.index].project_id
+}
+
+variable "link_project_policy_set_list" {
   type = list(object({
     policy_set_id = string
     project_id    = string
@@ -30,6 +59,39 @@ variable "project_policy_set_list" {
 
   default = []
 }
+
+variable "create_project_policy_set_list" {
+  type = list(object({
+    policy_set = object({
+    name                = string
+    description         = optional(string)
+    global         = optional(string)
+    kind                = optional(string)
+    agent_enabled       = optional(string)
+    policy_tool_version = optional(string)
+    overridable          = optional(bool)
+    organization        = optional(string)
+    policies_path       = optional(string)
+    description         = optional(string)
+    description         = optional(string)
+    description         = optional(string)
+    description         = optional(string)
+    workspace_ids       = optional(list(string))
+    })
+    project_id    = optional(string)
+  }))
+
+  default = []
+}
+
+# variable "project_policy_set_list" {
+#   type = list(object({
+#     policy_set_id = string
+#     project_id    = string
+#   }))
+
+#   default = []
+# }
 
 resource "tfe_project_variable_set" "project_variable_set" {
 
