5fb9d137cadba477460b68ee5ea59c825d9ce7d6
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-22 10:15:54 -0700

    Update Simple-Workspace-4.tf

--- examples/Simple-Workspace-4.tf
+++ examples/Simple-Workspace-4.tf
@@ -3,7 +3,7 @@ module "TFC-Project-Standalone-1" {
 
   create_workspace = false
   
-  organization                  = var.organization
+  # organization                  = var.organization
   name                          = "TFC-Project-Standalone-1"
   description                   = "Terraform project with no workspace 1"
   additional_tags_list            = ["who", "wants", "more", "features"]
