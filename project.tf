699a8498e179b3b015cf2f11b5a6b76e760daa8e
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-23 09:12:05 -0700

    Update project.tf

--- project.tf
+++ project.tf
@@ -36,9 +36,9 @@ variable "project_list" {
   default = []
 }
 
-# locals {
-#   create_policy_set_list = flatten([for p in var.project_list : p.policy_set_list])
-# }
+locals {
+  create_policy_set_list = flatten([for p in var.project_list : p.policy_set_list])
+}
 
 resource "tfe_policy_set" "policy_set" {
 
@@ -47,15 +47,13 @@ resource "tfe_policy_set" "policy_set" {
   # count = length(flatten(var.project_list[*].policy_set_list))
   # count = length(flatten(local.create_policy_set_list))
   # count = length(local.create_policy_set_list)
-  count = length(var.project_list.*.policy_set_list)
-
-
+  # count = length(var.project_list.*.policy_set_list)
   # for_each =  var.project_list.*.policy_set_list
-
+  count = length(local.create_policy_set_list)
 
   # name = each.value.policy_set.name
   # name = local.create_policy_set_list[count.index].name
-  name = var.project_list.*.policy_set_list[count.index].name
+  name = local.create_policy_set_list[count.index].name
 
   # name                = var.project_list.*.policy_set_list[count.index].policy_set.name
   # name                = var.project_list.*.policy_set_list[count.index].policy_set.name
