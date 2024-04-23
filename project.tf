c3d7c6b906d551ae2cda4e63ee9fe82f96636602
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-22 21:37:19 -0700

    Update project.tf

--- project.tf
+++ project.tf
@@ -52,36 +52,36 @@ resource "tfe_policy_set" "policy_set" {
 
   count = length(var.project_list[*].policy_set_list)
 
-  name                = var.create_project_policy_set_list[count.index].policy_set.name
-  description         = var.create_project_policy_set_list[count.index].policy_set.description
-  global              = var.create_project_policy_set_list[count.index].policy_set.global
-  kind                = var.create_project_policy_set_list[count.index].policy_set.kind
-  agent_enabled       = var.create_project_policy_set_list[count.index].policy_set.agent_enabled
-  policy_tool_version = var.create_project_policy_set_list[count.index].policy_set.policy_tool_version
-  overridable         = var.create_project_policy_set_list[count.index].policy_set.overridable
-  organization        = var.create_project_policy_set_list[count.index].policy_set.organization
-  policies_path       = var.create_project_policy_set_list[count.index].policy_set.policies_path
-  policy_ids          = var.create_project_policy_set_list[count.index].policy_set.policy_ids
+  name                = var.project_list[*].policy_set_list[count.index].policy_set.name
+  description         = var.project_list[*].policy_set_list[count.index].policy_set.description
+  global              = var.project_list[*].policy_set_list[count.index].policy_set.global
+  kind                = var.project_list[*].policy_set_list[count.index].policy_set.kind
+  agent_enabled       = var.project_list[*].policy_set_list[count.index].policy_set.agent_enabled
+  policy_tool_version = var.project_list[*].policy_set_list[count.index].policy_set.policy_tool_version
+  overridable         = var.project_list[*].policy_set_list[count.index].policy_set.overridable
+  organization        = var.project_list[*].policy_set_list[count.index].policy_set.organization
+  policies_path       = var.project_list[*].policy_set_list[count.index].policy_set.policies_path
+  policy_ids          = var.project_list[*].policy_set_list[count.index].policy_set.policy_ids
   dynamic "vcs_repo" {
-    for_each = var.create_project_policy_set_list
+    for_each = var.project_list[*].policy_set_list
     content {
-      identifier                 = var.create_project_policy_set_list[count.index].policy_set.vcs_repo.identifier
-      branch                     = var.create_project_policy_set_list[count.index].policy_set.vcs_repo.branch
-      ingress_submodules         = var.create_project_policy_set_list[count.index].policy_set.vcs_repo.ingress_submodules
-      oauth_token_id             = var.create_project_policy_set_list[count.index].policy_set.vcs_repo.oauth_token_id
-      github_app_installation_id = var.create_project_policy_set_list[count.index].policy_set.vcs_repo.github_app_installation_id
+      identifier                 = var.project_list[*].policy_set_list[count.index].policy_set.vcs_repo.identifier
+      branch                     = var.project_list[*].policy_set_list[count.index].policy_set.vcs_repo.branch
+      ingress_submodules         = var.project_list[*].policy_set_list[count.index].policy_set.vcs_repo.ingress_submodules
+      oauth_token_id             = var.project_list[*].policy_set_list[count.index].policy_set.vcs_repo.oauth_token_id
+      github_app_installation_id = var.project_list[*].policy_set_list[count.index].policy_set.vcs_repo.github_app_installation_id
     }
   }
-  workspace_ids = var.create_project_policy_set_list[count.index].policy_set.workspace_ids
-  slug          = var.create_project_policy_set_list[count.index].policy_set.slug
+  workspace_ids = var.project_list[*].policy_set_list[count.index].policy_set.workspace_ids
+  slug          = var.project_list[*].policy_set_list[count.index].policy_set.slug
 }
 
 resource "tfe_project_policy_set" "created" {
 
-  count = length(var.create_project_policy_set_list)
+  count = length(var.project_list[*].policy_set_list)
 
   policy_set_id = tfe_policy_set.policy_set[count.index].policy_set_id
-  project_id    = var.create_project_policy_set_list[count.index].project_id
+  project_id    = var.project_list[*].policy_set_list[count.index].project_id
 }
 
 resource "tfe_project_policy_set" "linked" {
@@ -100,7 +100,7 @@ variable "link_project_policy_set_list" {
   default = []
 }
 
-variable "create_project_policy_set_list" {
+variable "project_list[*].policy_set_list" {
   type = list(object({
     policy_set = object({
       name                = string
