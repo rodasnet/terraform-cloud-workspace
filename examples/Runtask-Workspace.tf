606e878437facafda5c53866343f5e2deba9d3bb
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-24 22:22:35 -0700

    Update Runtask-Workspace.tf

--- examples/Runtask-Workspace.tf
+++ examples/Runtask-Workspace.tf
@@ -7,10 +7,10 @@ module "Runtask-Workspace" {
   additional_tags_list            = ["synk", "poc"]
 
   run_task_config_list = [
-    {
-      task_id = data.tfe_organization_run_task.synk.id
-      stage = "pre_apply"
-    },
+    # {
+    #   task_id = data.tfe_organization_run_task.synk.id
+    #   stage = "pre_apply"
+    # },
     # {
     #   task_id = data.tfe_organization_run_task.synk.id
     #   stage = "post_plan"
