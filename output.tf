8befce04e7c41fd393700c103f1b1d569d24d234
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-24 23:55:29 -0700

    This object has no argument, nested block, or exported attribute named "workspace_ids".

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "workspace_id" {
   value = tfe_workspace.workspace[0].id
 }
 
-output "workspace_link_ids" {
-  value = tfe_project.project[0].workspace_ids
-}
+# output "workspace_link_ids" {
+#   value = tfe_project.project[0].workspace_ids
+# }
