6135159055d8b6db3d625318ef44393ad597d155
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-25 16:50:58 -0700

    Update Workspace-Standalone-1.tf

--- examples/Workspace-Standalone-1.tf
+++ examples/Workspace-Standalone-1.tf
@@ -4,11 +4,6 @@ module "Workspace-Standalone-1" {
   workspace_definition = {
     organization = var.organization
     name         = "Workspace-Standalone-1"
-    # policy_set_links = [
-    #   {
-    #     policy_set_id = module.Project-PolicySet-1.policy_sets[0].id
-    #   }
-    # ]
   }
 }
 
