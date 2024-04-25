d86777171eb2d91a0c30479323e1af17c0241804
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-24 23:54:00 -0700

    Update workspace_ids outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "workspace_id" {
-  value = tfe_workspace.workspace.*.id
+  value = tfe_workspace.workspace[0].id
+}
+
+output "workspace_link_ids" {
+  value = tfe_project.project[0].workspace_ids
 }
