ad7fcdde96e83f876315d5408d564681a93d6585
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-25 17:00:47 -0700

    Update Legacy-Workspace-Link-Project.tf

--- examples/Legacy-Workspace-Link-Project.tf
+++ examples/Legacy-Workspace-Link-Project.tf
@@ -2,7 +2,7 @@ module "Legacy-Workspace-Link-Project.tf" {
   source = "../archive/workspaces-only"
 
   organization         = var.organization
-  name                 = "Legacy-Workspace-Link-Project.tf"
+  name                 = "Legacy-Workspace-Link-Project"
   description          = "Test linking legacy workspace to project."
   additional_tags_list = ["project-link"]
   project_id           = module.Project-Workspace-Link.project_id
