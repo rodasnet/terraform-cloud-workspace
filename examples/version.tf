1c530bb837a0cafd1f86b6f8da3b751cdc7f8a85
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-26 13:51:53 -0700

    Update version.tf

--- examples/version.tf
+++ examples/version.tf
@@ -1,8 +1,8 @@
-terraform {
-  required_providers {
-    tfe = {
-      source  = "hashicorp/tfe"
-      version = "0.54.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     tfe = {
+#       source  = "hashicorp/tfe"
+#       version = "0.54.0"
+#     }
+#   }
+# }
