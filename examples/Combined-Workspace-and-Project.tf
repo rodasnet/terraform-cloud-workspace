6077a7a1b710595eea583d06e9604307f255bd47
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-04-26 15:53:40 -0700

    Update Combined-Workspace-and-Project.tf

--- examples/Combined-Workspace-and-Project.tf
+++ examples/Combined-Workspace-and-Project.tf
@@ -1,17 +1,17 @@
-module "Combined--Workspace-and-Project-Standalone-Beta" {
+module "Standalone-Workspace-and-Project-Beta" {
   source = "../"
 
   # note: both the tfc workspace and project resources are created as standalone
 
   workspace_definition = {
     organization = var.organization
-    name         = "Standalone-Workspace-and-Project-Beta"
+    name         = "Standalone-Workspace-Beta"
     description  = "Standalone workspace and project created in a single module call, currently in development."
   }
 
   project_definition = {
     organization = var.organization
-    name         = "Standalone-Project-and-Workspace-Beta"
+    name         = "Standalone-Project-Beta"
     description = "Standalone workspace and project created in a single module call, currently in development."
   }
 }
