e6eddc12ba03b723bd553febd9c83b2dc6142248
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-05-05 19:05:57 -0700

    Update project_team_access.tf

--- project_team_access.tf
+++ project_team_access.tf
@@ -1,17 +1,34 @@
 resource "tfe_team" "project_team" {
   
-  count = try(var.project_definition, null) != null && try(var.project_definition.teams, null) != null ? length(var.project_definition.teams) : 0
+  for_each = var.project_definition != null ? toset(var.project_definition.teams) : toset([])
 
-  name = var.project_definition.teams[count.index].name
+  name = each.value.name
   organization = "rodasnet"
 }
 
 resource "tfe_team_project_access" "project_access" {
 
-    # count = length(tfe_team.project_team)
-    count = try(var.project_definition, null) != null && try(var.project_definition.teams, null) != null ? length(var.project_definition.teams) : 0
+    for_each = var.project_definition != null ? toset(var.project_definition.teams) : toset([])
 
-    access       = var.project_definition.teams[count.index].access
-    team_id      = tfe_team.project_team[count.index].id
+    access       = each.value.access
+    team_id      = each.key
     project_id   = tfe_project.project[0].id
 }
+
+# resource "tfe_team" "project_team_count" {
+  
+#   count = try(var.project_definition, null) != null && try(var.project_definition.teams, null) != null ? length(var.project_definition.teams) : 0
+
+#   name = var.project_definition.teams[count.index].name
+#   organization = "rodasnet"
+# }
+
+# resource "tfe_team_project_access" "project_access_count" {
+
+#     # count = length(tfe_team.project_team)
+#     count = try(var.project_definition, null) != null && try(var.project_definition.teams, null) != null ? length(var.project_definition.teams) : 0
+
+#     access       = var.project_definition.teams[count.index].access
+#     team_id      = tfe_team.project_team[count.index].id
+#     project_id   = tfe_project.project[0].id
+# }
