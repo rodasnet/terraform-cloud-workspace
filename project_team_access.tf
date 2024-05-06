06421dcc58d5fbecfbc573aba690b9a7008dca18
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-05-05 19:14:27 -0700

    Update project_team_access.tf

--- project_team_access.tf
+++ project_team_access.tf
@@ -1,5 +1,5 @@
 locals {
-  project_teams = var.project_definition != null ? toset(var.project_definition.teams) : toset([])
+  project_teams = var.project_definition != null && var.project_definition.teams != null ? toset(var.project_definition.teams) : toset([])
 }
 resource "tfe_team" "project_team" {
   
