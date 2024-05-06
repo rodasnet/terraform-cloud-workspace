7e24e16844f910420b61ed9353e9692589ca234a
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-05-05 20:08:00 -0700

    Update project_team_access.tf

--- project_team_access.tf
+++ project_team_access.tf
@@ -17,21 +17,3 @@ resource "tfe_team_project_access" "project_access" {
     team_id      = each.key
     project_id   = tfe_project.project[0].id
 }
-
-# resource "tfe_team" "project_team_count" {
-  
-#   count = try(var.project_definition, null) != null && try(var.project_definition.teams, null) != null ? length(var.project_definition.teams) : 0
-
-#   name = var.project_definition.teams[count.index].name
-#   organization = "rodasnet"
-# }
-
-# resource "tfe_team_project_access" "project_access_count" {
-
-#     # count = length(tfe_team.project_team)
-#     count = try(var.project_definition, null) != null && try(var.project_definition.teams, null) != null ? length(var.project_definition.teams) : 0
-
-#     access       = var.project_definition.teams[count.index].access
-#     team_id      = tfe_team.project_team[count.index].id
-#     project_id   = tfe_project.project[0].id
-# }
