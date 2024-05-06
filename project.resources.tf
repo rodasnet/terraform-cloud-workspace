5ff0e1bc69a9b3261fb412df3a8e6b2c8e8851ae
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-05-06 11:52:54 -0700

    Update project.resources.tf

--- project.resources.tf
+++ project.resources.tf
@@ -60,8 +60,8 @@ locals {
 }
 resource "tfe_project_policy_set" "linked" {
 
-  for_each = toset(local.project_policy_set_links)
+  for_each = toset(keys({ for k,v in local.project_policy_set_links: k => v }))
 
-  policy_set_id = each.value.policy_set_id
+  policy_set_id = local.project_policy_set_links[each.key].policy_set_id
   project_id    = tfe_project.project[0].id
 }
