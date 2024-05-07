754e3d8b654c3106f0e95d8355edd841de5dcd52
Daniel Rodas <daniel.rodas1@hotmail.com>
2024-05-06 21:12:52 -0700

    Update project_team_access.tf

--- project_team_access.tf
+++ project_team_access.tf
@@ -3,18 +3,17 @@ locals {
 }
 resource "tfe_team" "project_team" {
   
-  for_each = toset(keys({ for k,v in local.project_teams: k => v })) 
+  for_each = { for k,v in local.project_teams: k => v }
 
-  name = local.project_teams[each.value].name
+  name = each.value.name
   organization = "rodasnet"
 }
 
 resource "tfe_team_project_access" "project_access" {
 
-    # for_each = toset(keys({ for k,v in local.project_teams: k => v })) 
     for_each = tfe_team.project_team
 
-    access       = local.project_teams[each.key].access
+    access       = each.value.access
     team_id      = each.value.id
     project_id   = tfe_project.project[0].id
 }
